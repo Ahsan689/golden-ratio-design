6cce6e7093fa9c5ab389979f82b8242d5239d707
ahsan689 <ahsanaleem689@gmail.com>
2024-04-22 00:44:32 +0500

    home section, portflio section modified

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,7 +8,8 @@ const Blog = () => {
     <div className="aali_tm_section" id="blog">
       <div className="aali_tm_news">
         <div className="container">
-          <div
+          
+          {/* <div
             className="aali_tm_main_title"
             data-text-align="center"
             data-color="dark"
@@ -19,21 +20,39 @@ const Blog = () => {
               We offer high quality products for competitive prices. Our main
               goal is customer satisfaction
             </p>
-          </div>
+          </div> */}
           <div className="news_list">
             <ul>
               <li className="wow fadeInLeft" data-wow-duration="1s">
                 <div className="list_inner">
                   <div className="image">
-                    <img src="img/thumbs/42-29.jpg" alt="aali image" />
-                    <div className="main" data-img-url="img/news/1.jpg" />
+                    <img src="DISPLAY_PICTURES/001.jpg" alt="aali image" />
+                    <div className="main" data-img-url="DISPLAY_PICTURES/001.jpg" />
                     <a
                       className="aali_tm_full_link"
                       href="#"
                       onClick={() => setModal(1)}
                     />
                   </div>
-                  <div className="details">
+                  <div className="image">
+                    <img src="DISPLAY_PICTURES/002.jpg" alt="aali image" />
+                    <div className="main" data-img-url="DISPLAY_PICTURES/002.jpg" />
+                    <a
+                      className="aali_tm_full_link"
+                      href="#"
+                      onClick={() => setModal(1)}
+                    />
+                  </div>
+                  <div className="image">
+                    <img src="DISPLAY_PICTURES/003.jpg" alt="aali image" />
+                    <div className="main" data-img-url="DISPLAY_PICTURES/003.jpg" />
+                    <a
+                      className="aali_tm_full_link"
+                      href="#"
+                      onClick={() => setModal(1)}
+                    />
+                  </div>
+                  {/* <div className="details">
                     <span className="category">
                       <a href="#">Web Design</a>
                     </span>
@@ -43,14 +62,14 @@ const Blog = () => {
                       </a>
                     </h3>
                     <span className="date">November 10, 2021</span>
-                  </div>
-                  <div className="button">
+                  </div> */}
+                  {/* <div className="button">
                     <div className="aali_tm_learn_more">
                       <a href="#" onClick={() => setModal(1)}>
                         Full Story
                       </a>
                     </div>
-                  </div>
+                  </div> */}
                   <div className={hidden(1)}>
                     <ModalBox close={setModal}>
                       <div className="news_popup_informations">
@@ -58,7 +77,15 @@ const Blog = () => {
                           <img src="img/thumbs/4-2.jpg" alt="aali image" />
                           <div className="main" data-img-url="img/news/1.jpg" />
                         </div>
-                        <div className="details">
+                        <div className="image">
+                          <img src="img/thumbs/4-2.jpg" alt="aali image" />
+                          <div className="main" data-img-url="img/news/1.jpg" />
+                        </div>
+                        <div className="image">
+                          <img src="img/thumbs/4-2.jpg" alt="aali image" />
+                          <div className="main" data-img-url="img/news/1.jpg" />
+                        </div>
+                        {/* <div className="details">
                           <span className="category">
                             <a href="#">Development</a>
                           </span>
@@ -93,7 +120,7 @@ const Blog = () => {
                             we can design a custom website that drives sales for
                             your unique business.`}
                           </p>
-                        </div>
+                        </div> */}
                       </div>
                     </ModalBox>
                   </div>
@@ -102,15 +129,33 @@ const Blog = () => {
               <li className="wow fadeInRight" data-wow-duration="1s">
                 <div className="list_inner">
                   <div className="image">
-                    <img src="img/thumbs/42-29.jpg" alt="aali image" />
-                    <div className="main" data-img-url="img/news/2.jpg" />
+                    <img src="DISPLAY_PICTURES/004.jpg" alt="aali image" />
+                    <div className="main" data-img-url="DISPLAY_PICTURES/004.jpg" />
                     <a
                       className="aali_tm_full_link"
                       href="#"
                       onClick={() => setModal(1)}
                     />
                   </div>
-                  <div className="details">
+                  <div className="image">
+                    <img src="DISPLAY_PICTURES/005.jpg" alt="aali image" />
+                    <div className="main" data-img-url="DISPLAY_PICTURES/005.jpg" />
+                    <a
+                      className="aali_tm_full_link"
+                      href="#"
+                      onClick={() => setModal(1)}
+                    />
+                  </div>
+                  <div className="image">
+                    <img src="DISPLAY_PICTURES/006.jpg" alt="aali image" />
+                     <div className="main" data-img-url="DISPLAY_PICTURES/006.jpg" />
+                    <a
+                      className="aali_tm_full_link"
+                      href="#"
+                      onClick={() => setModal(1)}
+                    />
+                  </div>
+                  {/* <div className="details">
                     <span className="category">
                       <a href="#">Development</a>
                     </span>
@@ -127,7 +172,7 @@ const Blog = () => {
                         Full Story
                       </a>
                     </div>
-                  </div>
+                  </div> */}
                   <div className={hidden(2)}>
                     <ModalBox close={setModal}>
                       <div className="news_popup_informations">
@@ -135,7 +180,7 @@ const Blog = () => {
                           <img src="img/thumbs/4-2.jpg" alt="aali image" />
                           <div className="main" data-img-url="img/news/2.jpg" />
                         </div>
-                        <div className="details">
+                        {/* <div className="details">
                           <span className="category">
                             <a href="#">Web Design</a>
                           </span>
@@ -172,7 +217,104 @@ const Blog = () => {
                             we can design a custom website that drives sales for
                             your unique business.`}
                           </p>
+                        </div> */}
+                      </div>
+                    </ModalBox>
+                  </div>
+                </div>
+              </li>
+              <li className="wow fadeInRight" data-wow-duration="1s">
+                <div className="list_inner">
+                  <div className="image">
+                   <img src="DISPLAY_PICTURES/007.jpg" alt="aali image" />
+                    <div className="main" data-img-url="DISPLAY_PICTURES/007.jpg" />
+                    <a
+                      className="aali_tm_full_link"
+                      href="#"
+                      onClick={() => setModal(1)}
+                    />
+                  </div>
+                  <div className="image">
+                   <img src="DISPLAY_PICTURES/008.jpg" alt="aali image" />
+                    <div className="main" data-img-url="DISPLAY_PICTURES/008.jpg" />
+                    <a
+                      className="aali_tm_full_link"
+                      href="#"
+                      onClick={() => setModal(1)}
+                    />
+                  </div>
+                  <div className="image">
+                   <img src="DISPLAY_PICTURES/009.jpg" alt="aali image" />
+                    <div className="main" data-img-url="DISPLAY_PICTURES/009.jpg" />
+                    <a
+                      className="aali_tm_full_link"
+                      href="#"
+                      onClick={() => setModal(1)}
+                    />
+                  </div>
+                  {/* <div className="details">
+                    <span className="category">
+                      <a href="#">Development</a>
+                    </span>
+                    <h3 className="title">
+                      <a href="#" onClick={() => setModal(2)}>
+                        What is mobile app and web design
+                      </a>
+                    </h3>
+                    <span className="date">October 22, 2021</span>
+                  </div>
+                  <div className="button">
+                    <div className="aali_tm_learn_more">
+                      <a href="#" onClick={() => setModal(2)}>
+                        Full Story
+                      </a>
+                    </div>
+                  </div> */}
+                  <div className={hidden(2)}>
+                    <ModalBox close={setModal}>
+                      <div className="news_popup_informations">
+                        <div className="image">
+                          <img src="img/thumbs/4-2.jpg" alt="aali image" />
+                          <div className="main" data-img-url="img/news/2.jpg" />
                         </div>
+                        {/* <div className="details">
+                          <span className="category">
+                            <a href="#">Web Design</a>
+                          </span>
+                          <h3 className="title">
+                            <a href="#">
+                              How to create simple personal website
+                            </a>
+                          </h3>
+                          <span className="date">October 22, 2021</span>
+                          <div />
+                        </div>
+                        <div className="text">
+                          <p>
+                            Aali is a leading web design agency with an
+                            award-winning design team that creates innovative,
+                            effective websites that capture your brand, improve
+                            your conversion rates, and maximize your revenue to
+                            help grow your business and achieve your goals.
+                          </p>
+                          <p>
+                            {`In today’s digital world, your website is the first
+                            interaction consumers have with your business.
+                            That's why almost 95 percent of a user’s first
+                            impression relates to web design. It’s also why web
+                            design services can have an immense impact on your
+                            company’s bottom line.`}
+                          </p>
+                          <p>
+                            {`That’s why more companies are not only reevaluating
+                            their website’s design but also partnering with
+                            Kura, the web design agency that’s driven more than
+                            $2.4 billion in revenue for its clients. With over
+                            50 web design awards under our belt, we're confident
+                            we can design a custom website that drives sales for
+                            your unique business.`}
+                          </p>
+                        </div> */}
                       </div>
                     </ModalBox>
                   </div>
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,21 @@
 import { useEffect } from "react";
 import { scroll_, stickyNav } from "../utilits";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { BsTiktok } from "react-icons/bs";
+import { FaWhatsapp } from "react-icons/fa";
+import { IoHomeOutline } from "react-icons/io5";
+import { BsFolder2Open } from "react-icons/bs";
+import { MdGroups } from "react-icons/md";
+import { MdMiscellaneousServices } from "react-icons/md";
+import { LuContact } from "react-icons/lu";
+
 
 const Header = ({ dark }) => {
   useEffect(() => {
-    window.addEventListener("scroll", stickyNav);
-    window.addEventListener("scroll", scroll_);
+    // window.addEventListener("scroll", stickyNav);
+    // window.addEventListener("scroll", scroll_);
   });
   return (
     <div className="aali_tm_header">
@@ -27,33 +38,33 @@ const Header = ({ dark }) => {
             </div>
           )}
 
+        </div>
+      </div>
           <div className="menu">
             <ul className="anchor_nav">
               <li className="current">
-                <a href="#home">Home</a>
+                <a href="#home"><IoHomeOutline /></a>
               </li>
               <li>
-                <a href="#about">About</a>
+                <a href="#about"><MdGroups/></a>
               </li>
               <li>
-                <a href="#portfolio">Portfolio</a>
+                <a href="#portfolio"><BsFolder2Open/></a>
               </li>
               <li>
-                <a href="#service">Service</a>
+                <a href="#service"><MdMiscellaneousServices/></a>
               </li>
-              <li>
+              {/* <li>
                 <a href="#testimonial">Testimonial</a>
               </li>
               <li>
                 <a href="#blog">Blog</a>
-              </li>
+              </li> */}
               <li>
-                <a href="#contact">Contact</a>
+                <a href="#contact"><LuContact/></a>
               </li>
             </ul>
           </div>
-        </div>
-      </div>
     </div>
   );
 };
